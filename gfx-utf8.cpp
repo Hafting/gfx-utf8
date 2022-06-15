875e51dcc92c3d6514aeb9c6ad5a2364c8a6251f
Helge Hafting <helge.hafting@ntnu.no>
2022-06-15 22:57:04 +0200

    drawChar_utf8() made

--- gfx-utf8.cpp
+++ gfx-utf8.cpp
@@ -1,5 +1,57 @@
 #include "gfx-utf8.h"
 
+//Pointer magic that might be needed:
+#ifdef __AVR__
+#include <avr/pgmspace.h>
+#elif defined(ESP8266) || defined(ESP32)
+#include <pgmspace.h>
+#endif
+
+#ifndef pgm_read_byte
+#define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+#endif
+#ifndef pgm_read_word
+#define pgm_read_word(addr) (*(const unsigned short *)(addr))
+#endif
+#ifndef pgm_read_dword
+#define pgm_read_dword(addr) (*(const unsigned long *)(addr))
+#endif
+
+// Pointers are a peculiar case...typically 16-bit on AVR boards,
+// 32 bits elsewhere.  Try to accommodate both...
+
+#if !defined(__INT_MAX__) || (__INT_MAX__ > 0xFFFF)
+#define pgm_read_pointer(addr) ((void *)pgm_read_dword(addr))
+#else
+#define pgm_read_pointer(addr) ((void *)pgm_read_word(addr))
+#endif
+
+inline GFXglyph *pgm_read_glyph_ptr(const GFXfont *gfxFont, uint16_t c) {
+#ifdef __AVR__
+  return &(((GFXglyph *)pgm_read_pointer(&gfxFont->glyph))[c]);
+#else
+  // expression in __AVR__ section may generate "dereferencing type-punned
+  // pointer will break strict-aliasing rules" warning In fact, on other
+  // platforms (such as STM32) there is no need to do this pointer magic as
+  // program memory may be read in a usual way So expression may be simplified
+  return gfxFont->glyph + c;
+#endif //__AVR__
+}
+
+inline uint8_t *pgm_read_bitmap_ptr(const GFXfont *gfxFont) {
+#ifdef __AVR__
+  return (uint8_t *)pgm_read_pointer(&gfxFont->bitmap);
+#else
+  // expression in __AVR__ section generates "dereferencing type-punned pointer
+  // will break strict-aliasing rules" warning In fact, on other platforms (such
+  // as STM32) there is no need to do this pointer magic as program memory may
+  // be read in a usual way So expression may be simplified
+  return gfxFont->bitmap;
+#endif //__AVR__
+}
+
+
+
 utf8::utf8(Adafruit_GFX *displ) { 
 	display = displ;
 	cur_fonts = 0;
@@ -29,9 +81,45 @@ void utf8::drawChar_utf8(int16_t x, int16_t y, uint16_t c, uint16_t color, uint8
 }
 
 void utf8::drawChar_utf8(int16_t x, int16_t y, uint16_t c, uint16_t color, uint8_t size_x, uint8_t size_y) {
-	GFXfont *gfxfont = font_lookup(c);
-	if (!gfxfont) return; //No font with this symbol, no printing!
+	GFXfont *gfxFont = font_lookup(c);
+	if (!gfxFont) return; //No font with this symbol, no printing!
 
+	//From Adafruit drawChar, with int8 replaced with int16 as needed
+	c -= (uint16_t)pgm_read_word(&gfxFont->first);
+	GFXglyph *glyph = pgm_read_glyph_ptr(gfxFont, c);
+	uint8_t *bitmap = pgm_read_bitmap_ptr(gfxFont);
+	
+
+	uint16_t bo = pgm_read_word(&glyph->bitmapOffset);
+	uint8_t w = pgm_read_byte(&glyph->width), h = pgm_read_byte(&glyph->height);
+	int8_t xo = pgm_read_byte(&glyph->xOffset),
+	       yo = pgm_read_byte(&glyph->yOffset);
+	uint8_t xx, yy, bits = 0, bit = 0;
+	int16_t xo16 = 0, yo16 = 0;
+
+	if (size_x > 1 || size_y > 1) {
+		xo16 = xo;
+		yo16 = yo;
+	}
+	
+	display->startWrite();
+	for (yy = 0; yy < h; yy++) {
+		for (xx = 0; xx < w; xx++) {
+			if (!(bit++ & 7)) {
+				bits = pgm_read_byte(&bitmap[bo++]);
+			}
+			if (bits & 0x80) {
+				if (size_x == 1 && size_y == 1) {
+					display->writePixel(x + xo + xx, y + yo + yy, color);
+				} else {
+					display->writeFillRect(x + (xo16 + xx) * size_x, y + (yo16 + yy) * size_y,
+					              size_x, size_y, color);
+				}
+			}
+			bits <<= 1;
+		}
+	}
+	display->endWrite();
 }
 
 
